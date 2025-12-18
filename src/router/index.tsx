6f37455cfbe99d053a30ce7f97bb686acf6413f1
Dang Vo <dang.vo@flynk.com>
2025-12-19 01:41:29 +0700

    fix deployment basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ],
   {
-    path: "/",
-    element: <Home />,
-  },
-]);
+    basename: "/Order-Product-App/",
+  }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Order-Product-App/",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("antd")) return "vendor-antd";
+            if (id.includes("@ant-design/icons")) return "vendor-icons";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
